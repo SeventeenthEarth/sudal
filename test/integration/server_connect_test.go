abc25554afbd4e6d5f5bf9325eb904f5e1ef22cc
17thearth <17thearth@gmail.com>
2025-08-31 18:16:08 +0900

    Enhance

--- test/integration/server_connect_test.go
+++ test/integration/server_connect_test.go
@@ -54,7 +54,7 @@ var _ = Describe("Server Connect Integration", func() {
 		if testServer != nil {
 			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 			defer cancel()
-			_ = testServer.Close(ctx)
+			Expect(testServer.Close(ctx)).To(Succeed())
 		}
 
 		// Reset config
